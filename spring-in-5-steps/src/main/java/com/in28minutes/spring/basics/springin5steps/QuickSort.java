d2d63a8ae5a64bb47eb5cc5bbdb5cfd2fbf49fc9
Vamshi Reddy <Vamshi Reddy@192.168.0.5>
2020-05-04 21:55:20 +0530

    Understanding the application context and @Component

--- spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/QuickSort.java
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/QuickSort.java
@@ -1,5 +1,7 @@
 package com.in28minutes.spring.basics.springin5steps;
 
+import org.springframework.stereotype.Component;
+
 public class QuickSort implements SortAlgorithm{
      public int[] sort(int[] numbers) {
     	 //logic for bubblesort
--- spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsApplication.java
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsApplication.java
@@ -2,15 +2,18 @@ package com.in28minutes.spring.basics.springin5steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringIn5StepsApplication {
 
 	public static void main(String[] args) {
-		BinarySearchImpl binarySearch=new BinarySearchImpl(new QuickSort());
+//		BinarySearchImpl binarySearch=new BinarySearchImpl(new QuickSort());
+		ApplicationContext applicationcontext=SpringApplication.run(SpringIn5StepsApplication.class, args);
+		BinarySearchImpl binarySearch=applicationcontext.getBean(BinarySearchImpl.class);
 		int result=binarySearch.binarySearch(new int[] {12,4,6},3);
 		System.out.println(result);
-//		SpringApplication.run(SpringIn5StepsApplication.class, args);
+
 	}
 
 }
