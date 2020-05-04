87cbc19bebbc7332934e9882244ebef9fbbf929c
Vamshi Reddy <Vamshi Reddy@192.168.0.5>
2020-05-04 13:36:49 +0530

    Coupling

--- spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsApplication.java
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsApplication.java
@@ -8,7 +8,8 @@ public class SpringIn5StepsApplication {
 
 	public static void main(String[] args) {
 		BinarySearchImpl binarySearch=new BinarySearchImpl();
-		binarySearch.binarySearch(new int[] {12,4,6},3);
+		int result=binarySearch.binarySearch(new int[] {12,4,6},3);
+		System.out.println(result);
 		SpringApplication.run(SpringIn5StepsApplication.class, args);
 	}
 
